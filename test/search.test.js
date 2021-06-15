5a71c1bc2fe6b096d684b92e970211e9a0d524e5
Jessica Kerr <jessitron@gmail.com>
2021-06-15 12:43:58 -0500

    Failing test reveals the list of ids

--- test/search.test.js
+++ test/search.test.js
@@ -1,6 +1,8 @@
 const search = require("../src/search");
 const { fakeEverything } = require("./fakes");
 
+const assert = require("assert");
+
 describe("searching", () => {
 
   it("can do a thing", () => {
@@ -10,6 +12,10 @@ describe("searching", () => {
     console.log("connection q: " + JSON.stringify(global.dbConnection.query, null, 2))
     global.dbConnection.query()
     const result = search("meow");
+
+    const ids = result.map(r => r.id);
+
+    assert.deepStrictEqual(ids, []);
 
   });
 });
