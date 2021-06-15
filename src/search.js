1f0698eda7acb41c7c6990b886c9a1fc44aa085f
Jessica Kerr <jessitron@gmail.com>
2021-06-15 13:01:26 -0500

    Exclude all private entries

--- src/search.js
+++ src/search.js
@@ -3,10 +3,15 @@ function search(phrase) {
   const entries = global.dbConnection.query(`select title, body, cat, visibility from
   entries where title || body like '%${phrase}%`);
 
-
-
-
-  return entries;
+  for (const i in entries) {
+    const e = entries[i];
+    console.log("visibility: " + e.visibility)
+    if (entries[i].visibility === "NONE") {
+      console.log("Deleting " + i)
+      delete entries[i];
+    }
+  }
+  return entries.filter(a => a);
 }
 
 module.exports = search;
