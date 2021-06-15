815ff976de3c07b3e6904abcf749d54a486aa4d2
Jessica Kerr <jessitron@gmail.com>
2021-06-15 13:50:16 -0500

    It works better with more duplication

--- src/search.js
+++ src/search.js
@@ -9,7 +9,12 @@ function search(phrase) {
     if (entries[i].visibility === "NONE" && e.cat !== session.cat.name) {
       console.log("Deleting " + i)
       entries.splice(i, 1);
-    } else if (entries[i].visibility === "FRIENDS" && !getFriends().includes(e.cat)) {
+    }
+  }
+  for (const i in entries) {
+    const e = entries[i];
+    console.log(" 2 checking entry: " + i + " which is: " + e.id)
+    if (entries[i].visibility === "FRIENDS" && !getFriends().includes(e.cat)) {
       console.log("Deleting " + i)
       entries.splice(i, 1);
     }
