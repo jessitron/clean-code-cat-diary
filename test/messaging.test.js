6952209f79b00f15be2054f7268f04707935a33e
Jessica Kerr <jessitron@gmail.com>
2021-06-17 17:22:32 -0500

    comments

--- test/messaging.test.js
+++ test/messaging.test.js
@@ -13,6 +13,14 @@ describe("sending a message", () => {
     const response = {};
 
     message(request, response);
+
+    // did it call the email service? How would we know?
+    // Does it work if the email service fails?
+    // Does it work if the cat is blocked by the recipient?
+    // how would we even trigger those tests?
+
+    const result = response.body;
+    assert.strictEqual(result, "Success");
   })
 
 
